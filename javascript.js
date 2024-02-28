0b4f6d0497bfcf48a59ef028e1823bde23146136
Jordan Gurian <gurianjordan@gmail.com>
2024-02-27 18:26:53 -0800

    Add grid resizing and div sizing improvement
    Grid will not expand beyond window, and dims can be changed

--- javascript.js
+++ javascript.js
@@ -1,23 +1,42 @@
 const container = document.querySelector('.container');
 
-let gridHeight = 16;
-let gridWidth = 16;
+const INITIAL_GRID_DIM = 16;
 const DIV_BORDER_WIDTH = 1;
 const DEFAULT_BODY_MARGIN = 8;
+const DEFAULT_BUTTON_HEIGHT = 21;
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight - DEFAULT_BUTTON_HEIGHT;
 
-document.onload = setGrid(gridHeight, gridWidth, DIV_BORDER_WIDTH, DEFAULT_BODY_MARGIN);
+let containerWidth = 0;
+
+if (windowWidth < windowHeight) {
+    containerWidth = windowWidth - ( 2 * DEFAULT_BODY_MARGIN );
+}
+else {
+    containerWidth = windowHeight - ( 2 * DEFAULT_BODY_MARGIN );
+}
+
+container.style.maxWidth = containerWidth + 'px';
+container.style.minWidth = containerWidth + 'px';
+
+let sizeButton = document.querySelector('button');
+
+sizeButton.addEventListener('click', function(){
+    deleteGrid();
+    do {
+        gridDim = Number(prompt("Input a new grid width, must be no more than 100!"));
+    } while (gridDim > 100)
+    setGrid(gridDim, DIV_BORDER_WIDTH, containerWidth);
+});
+
+document.onload = setGrid(INITIAL_GRID_DIM, DIV_BORDER_WIDTH, containerWidth);
 
 
-function setGrid(gridHeight, gridWidth, borderWidth, bodyMargin) {
-    let windowWidth = window.innerWidth;
-    let containerWidth = windowWidth - ( 2 * bodyMargin );
+function setGrid(gridDim, borderWidth, containerWidth) {
+    let divWidth = containerWidth / gridDim;
 
-    container.style.maxWidth = containerWidth + 'px';
-    container.style.minWidth = containerWidth + 'px';
-    let divWidth = containerWidth / gridWidth;
-
-    for (let i = 0; i < gridHeight; i++) {
-        for (let j = 0; j < gridWidth; j++) {
+    for (let i = 0; i < gridDim; i++) {
+        for (let j = 0; j < gridDim; j++) {
             let newDiv = document.createElement('div');
             newDiv.classList.add('pixels');
             newDiv.style.padding = (divWidth - ( 2 * borderWidth ) ) / 2 + "px";
@@ -27,4 +46,9 @@ function setGrid(gridHeight, gridWidth, borderWidth, bodyMargin) {
             container.appendChild(newDiv);
         }
     }
+}
+
+function deleteGrid() {
+    entireGrid = Array.from(document.querySelectorAll('.pixels'));
+    entireGrid.forEach(element => element.remove());
 }
