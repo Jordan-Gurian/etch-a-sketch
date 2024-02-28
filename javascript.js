86d28232b315cd112805787e36ef701a10b1cec8
Jordan Gurian <gurianjordan@gmail.com>
2024-02-27 19:37:06 -0800

    Add shading and random color button options
    Let user choose coloring style for etch-a-sketch

--- javascript.js
+++ javascript.js
@@ -1,11 +1,15 @@
 const container = document.querySelector('.container');
 
-const INITIAL_GRID_DIM = 16;
+let gridDim = 16;
 const DIV_BORDER_WIDTH = 1;
 const DEFAULT_BODY_MARGIN = 8;
 const DEFAULT_BUTTON_HEIGHT = 21;
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight - DEFAULT_BUTTON_HEIGHT;
+
+let TEN_PERCENT_RED = -255/10;
+let TEN_PERCENT_GREEN = -127/10;
+let TEN_PERCENT_BLUE = -255/10;
 
 let containerWidth = 0;
 
@@ -19,7 +23,9 @@ else {
 container.style.maxWidth = containerWidth + 'px';
 container.style.minWidth = containerWidth + 'px';
 
-let sizeButton = document.querySelector('button');
+let sizeButton = document.querySelector('.resize');
+let shadingButton = document.querySelector('.shading');
+let randomButton = document.querySelector('.random');
 
 sizeButton.addEventListener('click', function(){
     deleteGrid();
@@ -29,10 +35,22 @@ sizeButton.addEventListener('click', function(){
     setGrid(gridDim, DIV_BORDER_WIDTH, containerWidth);
 });
 
-document.onload = setGrid(INITIAL_GRID_DIM, DIV_BORDER_WIDTH, containerWidth);
+
+shadingButton.addEventListener('click', function(){
+    deleteGrid();
+    setGrid(gridDim, DIV_BORDER_WIDTH, containerWidth, 'shading');    
+});
+
+randomButton.addEventListener('click', function(){
+    deleteGrid();
+    setGrid(gridDim, DIV_BORDER_WIDTH, containerWidth, 'random');    
+});
 
 
-function setGrid(gridDim, borderWidth, containerWidth) {
+document.onload = setGrid(gridDim, DIV_BORDER_WIDTH, containerWidth);
+
+
+function setGrid(gridDim, borderWidth, containerWidth, colorType='none') {
     let divWidth = containerWidth / gridDim;
 
     for (let i = 0; i < gridDim; i++) {
@@ -40,9 +58,30 @@ function setGrid(gridDim, borderWidth, containerWidth) {
             let newDiv = document.createElement('div');
             newDiv.classList.add('pixels');
             newDiv.style.padding = (divWidth - ( 2 * borderWidth ) ) / 2 + "px";
-            newDiv.addEventListener('mouseenter', () => {
-                newDiv.style.backgroundColor = 'green';
-            })
+            newDiv.style.backgroundColor = 'rgb(255, 255, 255)'
+            switch (colorType) {
+                case 'shading':
+                    newDiv.addEventListener('mouseenter', () => {
+                        let rgbVec = getRGB(newDiv);
+                        let red = Number(rgbVec[0]);
+                        let green = Number(rgbVec[1]);
+                        let blue = Number(rgbVec[2]);
+                        if (green > 128) {
+                            newDiv.style.backgroundColor = `rgb(${red + TEN_PERCENT_RED}, ${green + TEN_PERCENT_GREEN}, ${blue + TEN_PERCENT_BLUE})`;
+                        }
+                    })
+                    break;
+                case 'random':
+                    newDiv.addEventListener('mouseenter', () => {
+                        newDiv.style.backgroundColor = `rgb(${Math.random() * 255 + 1}, ${Math.random() * 255 + 1}, ${Math.random() * 255 + 1})`;
+                    })  
+                    break;      
+                default:
+                    newDiv.addEventListener('mouseenter', () => {
+                        newDiv.style.backgroundColor = 'rgb(0, 128, 0)';
+                    })
+            }
+
             container.appendChild(newDiv);
         }
     }
@@ -51,4 +90,15 @@ function setGrid(gridDim, borderWidth, containerWidth) {
 function deleteGrid() {
     entireGrid = Array.from(document.querySelectorAll('.pixels'));
     entireGrid.forEach(element => element.remove());
+}
+
+// function deleteGridColor() {
+//     entireGrid = Array.from(document.querySelectorAll('.pixels'));
+//     entireGrid.forEach(element => element.style.backgroundColor = 'white');
+// }
+
+function getRGB(element) {
+    let rgbStr = element.style.backgroundColor;
+    rgbStr = rgbStr.substring(4, rgbStr.length - 1);
+    return rgbStr.split(', ');
 }
